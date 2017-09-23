0419f09f2b21c1151448b41b7b0136e230fe84bf
vixadd <dkroell@acm.org>
2017-09-23 01:06:29 -0400

    exporting project to github

---
+++ Project 2 - Arrays/src/array_functions.cpp
@@ -0,0 +1,78 @@
+/*
+ * functionstocomplete.cpp
+ *
+ *  Created on: Sep 10, 2017
+ *      Author: davidkroell
+ */
+
+//============================================================================
+//	TODO add necessary includes here
+//============================================================================
+
+#include "array_functions.h"
+
+using namespace std;
+
+
+struct Word {
+	string word;
+	int count = 0;
+};
+
+Word * wordPointer = new Word[10];
+int nextAvailableSlot = 0;
+
+void clearArray() {
+
+}
+
+int getArraySize() {
+
+	return 0;
+}
+
+string getArrayWordAt(int i) {
+
+	return "";
+}
+
+int getArrayWord_NumbOccur_At(int i) {
+	return 0;
+}
+
+bool processFile(fstream &myfstream) {
+
+	return false;
+}
+
+void processLine(string &myString) {
+
+}
+
+void processToken(string &token) {
+
+}
+
+bool openFile(fstream& myfile, const string& myFileName, ios_base::openmode mode) {
+
+	return false;
+
+}
+
+void closeFile(std::fstream& myfile) {
+
+}
+
+int writeArraytoFile(const std::string &outputfilename) {
+
+	return 0;
+}
+
+void sortArray(constants::sortOrder so) {
+
+}
+
+
+
+
+
