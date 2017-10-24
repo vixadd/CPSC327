0eebf7b13c4b2604684077963cb65cb572f5b677
vixadd <dkroell@acm.org>
2017-10-24 01:12:24 -0400

    adding cpp update

--- Project 3 - LSCCPSP/327_proj3_stringparser/StringParserClass.cpp
+++ Project 3 - LSCCPSP/327_proj3_stringparser/StringParserClass.cpp
@@ -5,12 +5,41 @@
  *      Author: keith
  */
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
 #include <string>
+#include <iterator>
 #include <string.h>
+
 #include "../327_proj3_test/includes/StringParserClass.h"
 
 KP_StringParserClass::StringParserClass::StringParserClass() {
+	pStartTag 		= '\0';
+	pEndTag   		= '\0';
+	areTagsSet 		= false;
+}
 
 
+
+KP_StringParserClass::StringParserClass::~StringParserClass() {
+	cleanup();
+}
+
+
+
+int KP_StringParserClass::StringParserClass::setTags(const char* pStart, const char* pEnd) {
+
+
+	return 0;
+}
+
+
+
+int KP_StringParserClass::StringParserClass::getDataBetweenTags(char *pDataToSearchThru,
+																std::vector<std::string> & myVector)
+{
+
+	return 0;
 }
 
