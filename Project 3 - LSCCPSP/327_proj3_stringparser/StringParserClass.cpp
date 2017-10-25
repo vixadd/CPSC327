bd256ee7e6010a9e73a9f27f6a710f7be16f6f0f
David Kroell <dkroell@acm.org>
2017-10-25 19:18:52 +0000

    Updating methods
    This commit updated the findTag and getDataBetweenTags functions.

--- Project 3 - LSCCPSP/327_proj3_stringparser/StringParserClass.cpp
+++ Project 3 - LSCCPSP/327_proj3_stringparser/StringParserClass.cpp
@@ -7,7 +7,6 @@
 
 #include <string>
 #include <string.h>
-
 #include "../327_proj3_test/includes/StringParserClass.h"
 #include "../327_proj3_test/includes/constants.h"
 
@@ -27,8 +26,8 @@ int KP_StringParserClass::StringParserClass::setTags(const char* pStart, const c
 	if(!pStart || !pEnd)
 		return ERROR_TAGS_NULL;
 
-	strcpy(pStartTag, pStart);
-	strcpy(pEndTag, pEnd);
+	pStartTag = strdup(pStart);
+	pEndTag = strdup(pEnd);
 
 
 	if(!pStartTag || !pEndTag) {
@@ -53,19 +52,112 @@ int KP_StringParserClass::StringParserClass::getDataBetweenTags(char *pDataToSea
 		return ERROR_DATA_NULL;
 	}
 
-	// TODO - Parse XML
+	myVector.clear();
+
+	// Pointers for the start tag
+	char *startTagStartPointer = pDataToSearchThru[0];
+	char *endTagStartPointer   = pDataToSearchThru[0];
+
+	// Pointers for the end tag.
+	char *startTagEndPointer   = pDataToSearchThru[0];
+	char *endTagEndPointer     = pDataToSearchThru[0];
+
+	// Find the location of the start and end pointer.
+	int mFindStartTag = findTag(pStartTag, startTagStartPointer, endTagStartPointer);
+	int mFindEndTag   = findTag(pEndTag, startTagEndPointer, endTagEndPointer);
+
+	// While the start tag and end tag are in the string we're looking into,
+	// Then extract data between the tags.
+	while(mFindStartTag == SUCCESS && mFindEndTag == SUCCESS) {
+
+		// Initialize the iteration pointer to the character after
+		// the '>' in our start tag, and initialize the string to be
+		// inserted into the vector to an empty string.
+		char *itrPtr = *(endTagStartPointer+1);
+		std::string insString = "";
+
+		// Extract the given string between the pointer
+		// after the end of the start tag, and before
+		// the start of the end tag.
+		while (itrPtr != startTagEndPointer) {
+			insString += *itrPtr;
+			itrPtr++;
+		}
+
+		// Push the resulting insertion string to the vector.
+		myVector.push_back(insString);
+
+		// Reset the pointer to occure after the tag insertion we just completed
+		// And rerun the find tags to find a new tag.
+		startTagStartPointer = (endTagEndPointer+1);
+		endTagStartPointer   = (endTagEndPointer+1);
+		startTagEndPointer   = (endTagEndPointer+1);
+		endTagEndPointer     = (endTagEndPointer+1);
+
+		// Rerun the start and end tag to find a new tag position
+		// from the new starting point.
+		mFindStartTag = findTag(pStartTag, startTagStartPointer, endTagStartPointer);
+		mFindEndTag   = findTag(pEndTag, startTagEndPointer, endTagEndPointer);
+	}
 
 	return SUCCESS;
 }
 
 void KP_StringParserClass::StringParserClass::cleanup()
 {
-	// Do Nothing
+	// Do Nothing???
 }
 
 int KP_StringParserClass::StringParserClass::findTag(char *pTagToLookFor, char *&pStart, char *&pEnd)
 {
-	return FAIL;
+	if (!pEnd || !pStart) {
+		return FAIL;
+	}
+
+	char* mTag = "";
+
+	// Find location of the starttag
+	while (pStart != 0) {
+
+		if (pStart == '<') {
+
+			// Store the start of the pointer temporarily.
+			char *& tempStart = pStart;
+
+			// Find the End of the tag.
+			while(pStart != 0) {
+
+				// Store the tag contents in mTag
+				mTag += *pStart;
+
+				// Increment the startpointer.
+				pStart += 1;
+
+				// We have reached the end of the tag.
+				if (pStart == '>') {
+
+					// Set the values of the start and end of the tag itself.
+					pStart = tempStart;
+					pEnd = pStart;
+					break;
+				}
+
+			}
+
+			// If the tag we found is equal to the sought after tag
+			// Then break the loop. We found the Tag.
+			if(mTag == pTagToLookFor)
+				break; // We have the start and end pointer of the tag.
+			else
+				mTag = "";
+		}
+		pStart += 1;
+	}
+
+	if (mTag != "")
+		return SUCCESS;
+	else
+		return FAIL;
 }
 
 
