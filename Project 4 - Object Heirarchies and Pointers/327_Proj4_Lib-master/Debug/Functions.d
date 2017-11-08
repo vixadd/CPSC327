b48ed4872072328b1f0afd0fdda709b736e475c7
vixadd <dkroell@acm.org>
2017-11-08 09:14:11 -0500

    init P4

---
+++ Project 4 - Object Heirarchies and Pointers/327_Proj4_Lib-master/Debug/Functions.d
@@ -0,0 +1,23 @@
+Functions.o: ../Functions.cpp .././includes/Functions.h \
+ .././includes/../../327_Proj4_Lib/includes/Smalltalk.h \
+ .././includes/../../327_Proj4_Lib/includes/Watch.h \
+ .././includes/Smalltalk_American.h \
+ .././includes/ST_American_DonutEnthusiest.h \
+ .././includes/Smalltalk_Brit.h .././includes/Watch.h \
+ .././includes/constants.h
+
+.././includes/Functions.h:
+
+.././includes/../../327_Proj4_Lib/includes/Smalltalk.h:
+
+.././includes/../../327_Proj4_Lib/includes/Watch.h:
+
+.././includes/Smalltalk_American.h:
+
+.././includes/ST_American_DonutEnthusiest.h:
+
+.././includes/Smalltalk_Brit.h:
+
+.././includes/Watch.h:
+
+.././includes/constants.h:
