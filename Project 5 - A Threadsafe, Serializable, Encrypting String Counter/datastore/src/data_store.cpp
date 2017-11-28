ce49b806f6b653ae99d8ea1572399564c1116cff
vixadd <dkroell@acm.org>
2017-11-28 03:56:03 -0500

    Updated skeleton functions for impl
    Skeleton of the functions are updated. Focus on impl next here.

--- Project 5 - A Threadsafe, Serializable, Encrypting String Counter/datastore/src/data_store.cpp
+++ Project 5 - A Threadsafe, Serializable, Encrypting String Counter/datastore/src/data_store.cpp
@@ -10,6 +10,39 @@
  * 		Version: 11/28/2017
  */
 
-#include "./includes/data_store.h"
+#include "../includes/data_store.h"
 
 
+DataStore::DataStore(Crypto *pCrypt)
+:myCrypto(pCrypt)
+{}
+
+DataStore::~DataStore(void)
+{
+	// Manage memory with obj destruction
+	if (myCrypto != 0) {
+		delete myCrypto;
+	}
+}
+
+bool DataStore::load(std::vector<String_Data> &myVector)
+{
+	return false;
+}
+
+bool DataStore::save(std::vector<String_Data> &myVector)
+{
+	return false;
+}
+
+// Protected functions below
+
+bool DataStore::decrypt(std::string &myString)
+{
+	return false;
+}
+
+bool DataStore::encrypt(std::string &myString)
+{
+	return false;
+}
