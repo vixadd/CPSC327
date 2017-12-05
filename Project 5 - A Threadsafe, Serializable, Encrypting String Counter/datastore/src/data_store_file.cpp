1b486460748e986772ed0139e38ba1038f3b48b5
vixadd <dkroell@acm.org>
2017-12-05 16:42:05 -0500

    Inintialized skeleton project files
    Project 5 requires class implmentations. Ready for skeleton file implmenetation now.

--- Project 5 - A Threadsafe, Serializable, Encrypting String Counter/datastore/src/data_store_file.cpp
+++ Project 5 - A Threadsafe, Serializable, Encrypting String Counter/datastore/src/data_store_file.cpp
@@ -1,3 +1,39 @@
+/*
+ * Data Store file for the abstract impl
+ * of Data store.
+ *
+ * Author: David Kroell
+ * Version: 12/04/2017
+ */
 
+#include "../includes/data_store_file.h"
 
-// TODO - Please develop the implmentation (data_store_file.h)
+DataStore_File::DataStore_File(std::string filename, Crypto* crypto)
+:DataStore(crypto),
+ myFileName(filename)
+{}
+
+DataStore_File::~DataStore_File()
+{
+
+}
+
+bool DataStore_File::load(std::vector<String_Data> &myVector)
+{
+	return false;
+}
+
+bool DataStore_File::save(std::vector<String_Data> &myVector)
+{
+	return false;
+}
+
+bool DataStore_File::openFile(std::fstream& myfile,const std::string& myFileName, std::ios_base::openmode mode)
+{
+	return false;
+}
+
+void DataStore_File::closeFile(std::fstream& myfile)
+{
+
+}
--- Project 5 - A Threadsafe, Serializable, Encrypting String Counter/stringdatabase/src/string_database.cpp
+++ Project 5 - A Threadsafe, Serializable, Encrypting String Counter/stringdatabase/src/string_database.cpp
@@ -1,3 +1,39 @@
+/*
+ * String database impl for the Project
+ * extracted from header.
+ *
+ * Author: David Kroell
+ * Version: 12/04/2017
+ */
 
+#include "../includes/string_database.h"
 
-// TODO - Develop the implmentation (string_database.h)
+String_Database::String_Database()
+{
+
+}
+
+void String_Database::add(std::string &myString)
+{
+
+}
+
+int String_Database::getCount(std::string &myString)
+{
+	return 0;
+}
+
+void String_Database::clear()
+{
+
+}
+
+bool String_Database::load(DataStore  *myDataStore)
+{
+	return false;
+}
+
+bool String_Database::save(DataStore *myDataStore)
+{
+	return false;
+}
