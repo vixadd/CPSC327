1961c8bd9d7f0cbf5def22ade84b57c6dcfeaf99
vixadd <dkroell@acm.org>
2017-12-06 03:08:14 -0500

    Data Store classes are initialized
    Data store calsses are finished for use by the string database class.

--- Project 5 - A Threadsafe, Serializable, Encrypting String Counter/datastore/src/data_store_file.cpp
+++ Project 5 - A Threadsafe, Serializable, Encrypting String Counter/datastore/src/data_store_file.cpp
@@ -8,10 +8,14 @@
 
 #include "../includes/data_store_file.h"
 
+#include <fstream>
+
 DataStore_File::DataStore_File(std::string filename, Crypto* crypto)
 :DataStore(crypto),
  myFileName(filename)
-{}
+{
+
+}
 
 DataStore_File::~DataStore_File()
 {
@@ -20,20 +24,57 @@ DataStore_File::~DataStore_File()
 
 bool DataStore_File::load(std::vector<String_Data> &myVector)
 {
-	return false;
+	std::fstream thisFile;
+
+	if(this->openFile(thisFile, this->myFileName, std::ios_base::in)) {
+		std::string str;
+		while(std::getline(thisFile, str)) {
+			if(this->myCrypto){
+				bool encrypted = myCrypto->decrypt(str);
+			}
+
+			myVector.push_back(str);
+		}
+
+		this->closeFile(thisFile);
+		return true;
+
+	} else {
+		return false;
+	}
 }
 
 bool DataStore_File::save(std::vector<String_Data> &myVector)
 {
-	return false;
+	std::fstream thisFile;
+	if(this->openFile(thisFile, this->myFileName, std::ios_base::out)) {
+
+		for(std::string str: myVector) {
+			if(this->myCrypto) {
+				bool encrypted = this->myCrypto->encrypt(str);
+			}
+
+			thisFile << str << std::endl;
+		}
+
+		thisFile.close();
+		return true;
+
+	} else {
+		return false;
+	}
+
 }
 
 bool DataStore_File::openFile(std::fstream& myfile,const std::string& myFileName, std::ios_base::openmode mode)
 {
-	return false;
+	myfile.open(myFileName.c_str(), mode);
+	return myfile.is_open();
 }
 
 void DataStore_File::closeFile(std::fstream& myfile)
 {
-
+	if(myfile.is_open()) {
+		myfile.close();
+	}
 }
